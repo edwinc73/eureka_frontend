ef98b77d105667dcc3537426f20d371ab5a1bf22
fanxin li <lexxshinjj@gmail.com>
2023-07-07 22:59:07 +0800

    authentiction

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    const app = this
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: `${app.globalData.baseUrl}/login`, 
+          method: 'post',
+          data: { code: res.code }, 
+          success(loginRes) {
+            app.globalData.user = loginRes.data.user
+            app.globalData.header = loginRes.data.headers
+          },
+        })
       }
     })
-  },
+  }
+
   globalData: {
-    userInfo: null
+    userInfo: null,
+    baseUrl: 'http://127.0.0.1:3000/api/v1'
   }
 })
