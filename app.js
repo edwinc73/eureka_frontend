e9911fdacc2bcc88fb1fb52a5f8013183550721c
Erica Yin <ericayin0225@gmail.com>
2023-07-08 21:24:25 +0800

    Revert "added global stylesheet, search component and the bottom layer component"
    This reverts commit 182d304144bea9b5b089245df01f9b91b6a2f2d9.

--- app.js
+++ app.js
@@ -2,42 +2,27 @@
 App({
   onLaunch() {
     // 展示本地存储能力
-    // const logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
 
-    // const app = this
+    const app = this
     // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //     wx.request({
-    //       url: `${app.globalData.baseUrl}/login`, 
-    //       method: 'post',
-    //       data: { code: res.code }, 
-    //       success(loginRes) {
-    //         app.globalData.user = loginRes.data.user
-    //         app.globalData.header = loginRes.data.headers
-    //       },
-    //     })
-    //   }
-    // })
-
-    wx.loadFontFace({
-      family:"quicksand",
-      global:true,
-      source: 'url("./Quicksand.ttf")',
-      desc: {
-        style: "normal",
-        weight: "normal",
-        variant: "normal"
-      },
-      complete: (msg) => {
-        console.log(msg)
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: `${app.globalData.baseUrl}/login`, 
+          method: 'post',
+          data: { code: res.code }, 
+          success(loginRes) {
+            app.globalData.user = loginRes.data.user
+            app.globalData.header = loginRes.data.headers
+          },
+        })
       }
-     
     })
-  },
+  }
 
   globalData: {
     userInfo: null,
