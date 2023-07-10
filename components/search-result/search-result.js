f1dcf2bcfae75f5ec5e7c7faed16a545d0c6bc34
Edwin Cheng <edwincheng09@googlemail.com>
2023-07-10 20:59:35 +0800

    added request for suggestions

--- components/search-result/search-result.js
+++ components/search-result/search-result.js
@@ -1,66 +1,26 @@
-// components/search-result/search-result.js
-Page({
-
-  /**
-   * Page initial data
-   */
-  data: {
-
+Component({
+  properties: {
+    data: {
+      type: Object,
+      value: {
+        carbs: 60,
+        category: "",
+        created_at: "",
+        description: "Classic Italian pasta dish with creamy sauce and bacon",
+        fat: 40,
+        fiber: null,
+        instructions: "1. Cook the pasta in a large pot of salted boiling water until al dente.↵2. In a skillet, cook the bacon until crispy. Remove from heat and set aside.↵3. In a bowl, whisk together the eggs, grated cheese, and black pepper.↵4. Drain the pasta, reserving a small amount of the cooking water.↵5. While the pasta is still hot, add it to the skillet with the cooked bacon. Toss to combine.↵6. Gradually pour the egg mixture over the hot pasta, stirring quickly to coat the pasta evenly.↵7. If the sauce is too thick, add a small amount of the reserved cooking water to thin it out.↵8. Serve immediately with extra grated cheese on top.",
+        name: "Pasta Carbonara",
+        protein: 30,
+        sodium: null,
+        total_calories: 700,
+        updated_at: "2023/07/10"
+      }
+    }
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage() {
-
+  methods: {
+    handleClick() {
+      console.log('Button Clicked!');
+    }
   }
-})
+});
